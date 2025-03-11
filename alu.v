468a2943f2c52bcbe1e9a7ed3aef2de7d83f8c75
Luigi C. Filho <67793599+luigicfilho@users.noreply.github.com>
2025-03-11 13:43:01 +0000

    Update with extra operations
    Find a new version of the alu

--- alu.v
+++ alu.v
@@ -21,15 +21,17 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module alu(
-	  rega,
-	  regb,
-	  control,
-	  out_alu,
-	  cout
-		);
+			rega,
+			regb,
+			control,
+			out_alu,
+			cout,
+			equal,
+			zero
+			);
 	
 parameter DATA_WITH = 16;
-parameter OP_SIZE = 3;
+parameter OP_SIZE = 4;
 
 `define ADD 4'd0
 `define SUB 4'd1
@@ -38,7 +40,15 @@ parameter OP_SIZE = 3;
 `define XOR 4'd4
 `define L_SH 4'd5
 `define R_SH 4'd6
-`define NOR 4'd7
+`define NAND 4'd7
+`define NOR 4'd8
+`define XNOR 4'd9
+`define NOT 4'd10
+`define COMP 4'd11
+`define ADDO 4'd12
+`define SUBO 4'd13
+`define SIG 4'd14
+`define SOME 4'd15
 `define PRE 17
 `define PRE_ZERO 17'd0
 `define ONE_BIT_ONE 1'b1
@@ -49,6 +59,8 @@ parameter OP_SIZE = 3;
 	input 		[OP_SIZE-1:0] 	control;
     output reg 	[DATA_WITH-1:0]	out_alu;
     output reg 					cout;
+	output reg 					zero;
+	output reg					equal;
 
 	reg 		[DATA_WITH+1:0]	pre_out;
 	
@@ -62,7 +74,20 @@ begin
 		`XOR  : pre_out = rega ^ regb; //4
 		`L_SH : pre_out = rega << regb; //5
 		`R_SH : pre_out = rega >> regb; //6
-		`NOR  : pre_out = ~(rega | regb); //7
+		`NAND : pre_out = ~(rega & regb); //7
+		`NOR  : pre_out = ~(rega | regb); //8
+		`XNOR : pre_out = ~(rega ^ regb); //9
+		`NOT  : pre_out[DATA_WITH-1:0] = ~rega; //10
+		`COMP : begin //11
+					if ( rega == regb )
+						equal = 1'b1;
+					else
+						equal = 1'b0;
+				end
+		`ADDO : pre_out = rega + 16'd1; //12
+		`SUBO : pre_out = rega - 16'd1; //13
+		`SIG  : pre_out = (~rega) + 16'd1; //14
+		`SOME : pre_out = rega + (~regb); //15
 	endcase
 end
 
@@ -70,6 +95,10 @@ always @(pre_out)
 begin
 		out_alu = pre_out[DATA_WITH-1:0];
 		cout = pre_out[`PRE];
+		if (pre_out == `PRE_ZERO)
+			zero = `ONE_BIT_ONE;
+		else
+			zero = `ONE_BIT_ZERO;
 end		
 
 endmodule
